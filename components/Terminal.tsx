fb988b3346608b77d86ae70b3a2a3c20e38ef052
João Barreiros <unstablectrl@gmail.com>
2021-08-12 11:09:06 +0100

    Fix echo syntax message - Exclamation mark escapes double quotes

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -66,7 +66,7 @@ const Terminal: React.FC<TerminalProps> = ({ className, style }) => {
       </header>
       <section className="py-3">
         <span>
-          {'⚡️ '} <strong>~</strong> {' echo "I\'m Unstable!"'}
+          {'⚡️ '} <strong>~</strong> {' echo "I\'m Unstable"'}
         </span>
       </section>
     </div>
