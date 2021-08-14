4d5da3770db10b3c9a3cbb562f49ea7a8f007075
João Barreiros <unstablectrl@gmail.com>
2021-08-14 13:08:18 +0100

    Add system theme mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-screen bg-gray-800 text-gray-50 grid place-items-center px-4 sm:px-8 md:px-12 lg:px-16">
+      <main className="h-screen bg-fuchsia-100 bg- dark:bg-gray-800 text-gray-50 grid place-items-center px-4 sm:px-8 md:px-12 lg:px-16">
         <div className="max-w-2xl w-full">
           <Animation3d>
             <AspectRatioBox aspectRatio={5 / 3}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
       mono: ['Fira Code', 'monospace'],
     },
     colors: {
       gray: colors.trueGray,
+      fuchsia: colors.fuchsia,
     },
   },
   variants: {
